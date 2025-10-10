be52ea2b6d5d7c3f22d252f12d204f96d0a698c1
CrisT777-JN <151756560+CrisT777-JN@users.noreply.github.com>
2025-10-10 11:36:12 +0800

    Add files via upload

---
+++ data/dataset_wogt.py
@@ -0,0 +1,71 @@
+import os.path
+import random
+import numpy as np
+import torch
+import cv2
+import torch.utils.data as data
+import utils.utils_image as util
+
+
+class Dataset(data.Dataset):
+    """
+    # -----------------------------------------
+    # Get L/H for denosing on AWGN with fixed sigma.
+    # Only dataroot_H is needed.
+    # -----------------------------------------
+    # e.g., DnCNN
+    # -----------------------------------------
+    """
+
+    def __init__(self, opt):
+        super(Dataset, self).__init__()
+        self.opt = opt
+        self.n_channels = opt['n_channels'] if opt['n_channels'] else 3
+        self.patch_size = opt['H_size'] if opt['H_size'] else 64
+        self.sigma = opt['sigma'] if opt['sigma'] else 25
+        self.sigma_test = opt['sigma_test'] if opt['sigma_test'] else self.sigma
+
+        # ------------------------------------
+        # get path of H
+        # return None if input is None
+        # ------------------------------------
+        self.paths_A = util.get_image_paths(opt['dataroot_A'])
+        self.paths_B = util.get_image_paths(opt['dataroot_B'])
+
+
+    def __getitem__(self, index):
+        A_path = self.paths_A[index]
+        B_path = self.paths_B[index]
+
+
+        img_A = cv2.imread(A_path,0)
+        img_A = np.expand_dims(img_A, axis=2)
+        img_B = cv2.imread(B_path,0)
+        img_B = np.expand_dims(img_B, axis=2)
+
+        # print('img.shape',img_A.shape,img_B.shape)
+
+        if self.opt['phase'] == 'train':
+            H, W, _ = img_A.shape
+            rnd_h = random.randint(0, max(0, H - self.patch_size))
+            rnd_w = random.randint(0, max(0, W - self.patch_size))
+
+            patch_A = img_A[rnd_h:rnd_h + self.patch_size, rnd_w:rnd_w + self.patch_size,:]
+            patch_B = img_B[rnd_h:rnd_h + self.patch_size, rnd_w:rnd_w + self.patch_size,:]
+            mode = random.randint(0,7)
+            patch_A, patch_B = util.augment_img(patch_A, mode=mode), util.augment_img(patch_B, mode=mode)
+            img_A = torch.from_numpy(np.ascontiguousarray(patch_A)).permute(2, 0, 1).float().div(255.)
+            img_B = torch.from_numpy(np.ascontiguousarray(patch_B)).permute(2, 0, 1).float().div(255.)
+
+            return {'A': img_A, 'B': img_B, 'A_path': A_path, 'B_path': B_path}
+
+        else:
+            img_A = np.float32(img_A/255.)
+            img_B = np.float32(img_B/255.)
+            img_A = torch.from_numpy(np.ascontiguousarray(img_A)).permute(2, 0, 1).float()
+            img_B = torch.from_numpy(np.ascontiguousarray(img_B)).permute(2, 0, 1).float()
+
+            return {'A': img_A, 'B': img_B, 'A_path': A_path, 'B_path': B_path}
+
+    def __len__(self):
+        return len(self.paths_A)
---
+++ train.py
@@ -0,0 +1,168 @@
+import os.path
+import math
+import argparse
+import time
+import random
+import numpy as np
+from collections import OrderedDict
+import logging
+from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
+import torch
+
+from utils import utils_logger
+from utils import utils_image as util
+from utils import utils_option as option
+from utils.utils_dist import get_dist_info, init_dist
+
+from data.select_dataset import define_Dataset
+from data.test_dataloder import Dataset as test_dataset
+from models.select_model import define_Model
+import warnings
+from models.loss_vif import fusion_loss_vif
+warnings.filterwarnings("ignore")
+testloss = fusion_loss_vif()
+
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+best_loss =100000000.0
+def main(json_path='config.json'):
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--opt', type=str, default=json_path, help='Path to option JSON file.')
+    parser.add_argument('--launcher', default='pytorch', help='job launcher')
+    parser.add_argument('--local_rank', type=int, default=0)
+    parser.add_argument('--dist', default=False)
+    opt = option.parse(parser.parse_args().opt, is_train=True)
+    opt['dist'] = parser.parse_args().dist
+
+    # ----------------------------------------
+    # distributed settings
+    # ----------------------------------------
+
+    if opt['dist']:
+        init_dist('pytorch')
+    opt['rank'], opt['world_size'] = get_dist_info()
+
+    if opt['rank'] == 0:
+        for key, path in opt['path'].items():
+            print(path)
+        util.mkdirs((path for key, path in opt['path'].items() if 'pretrained' not in key))
+
+    border = opt['scale']
+
+    if opt['rank'] == 0:
+        option.save(opt)
+    opt = option.dict_to_nonedict(opt)
+    if opt['rank'] == 0:
+        logger_name = 'train'
+        utils_logger.logger_info(logger_name, os.path.join(opt['path']['log'], logger_name + '.log'))
+        logger = logging.getLogger(logger_name)
+        logger.info(option.dict2str(opt))
+
+    # ----------------------------------------
+    # seed
+    # ----------------------------------------
+    seed = None
+    if seed is None:
+        seed = random.randint(1, 10000)
+    print('Random seed: {}'.format(seed))
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+    for phase, dataset_opt in opt['datasets'].items():
+        if phase == 'train':
+            train_set = define_Dataset(dataset_opt)
+            train_size = int(math.ceil(len(train_set) / dataset_opt['dataloader_batch_size']))
+            if opt['rank'] == 0:
+                logger.info('Number of train images: {:,d}, iters: {:,d}'.format(len(train_set), train_size))
+            if opt['dist']:
+                train_sampler = DistributedSampler(train_set, shuffle=dataset_opt['dataloader_shuffle'], drop_last=True,
+                                                   seed=seed)
+                train_loader = DataLoader(train_set,
+                                          batch_size=dataset_opt['dataloader_batch_size'] // opt['num_gpu'],
+                                          shuffle=False,
+                                          num_workers=dataset_opt['dataloader_num_workers'] // opt['num_gpu'],
+                                          drop_last=True,
+                                          pin_memory=True,
+                                          sampler=train_sampler)
+            else:
+                train_loader = DataLoader(train_set,
+                                          batch_size=dataset_opt['dataloader_batch_size'],
+                                          shuffle=dataset_opt['dataloader_shuffle'],
+                                          num_workers=dataset_opt['dataloader_num_workers'],
+                                          drop_last=True,
+                                          pin_memory=True)
+
+        elif phase == 'test':
+            test_set = define_Dataset(dataset_opt)
+            # test_set = test_dataset(dataset_opt)
+            test_loader = DataLoader(test_set, batch_size=1,
+                                     shuffle=False, num_workers=1,
+                                     drop_last=False, pin_memory=True)
+        else:
+            raise NotImplementedError("Phase [%s] is not recognized." % phase)
+
+    global best_loss
+    model = define_Model(opt)
+    model.init_train()
+
+
+    if opt['datasets']['dataset_type'] in ['mef_GT', 'mff_GT']:
+        need_GT = True
+    else:
+        need_GT = False
+    for epoch in range(300):  # keep running
+        for i, train_data in enumerate(train_loader):
+
+            model.update_learning_rate(epoch)
+            model.feed_data(train_data, need_GT=need_GT)
+            model.optimize_parameters(epoch)
+        logs = model.current_log()  # such as loss
+        message = '<epoch:{:3d}, lr:{:.3e}> '.format(epoch, model.current_learning_rate())
+        for k, v in logs.items():  # merge log information into message
+                message += '{:s}: {:.3e} '.format(k, v)
+        logger.info(message)
+
+        if epoch % 1 == 0:
+
+            avg_psnr = 0.0
+            idx = 0
+            img_dir = os.path.join(opt['path']['images'], '{:d}'.format(epoch))
+            loss = 0.0
+            for test_data in test_loader:
+                idx += 1
+                image_name_ext = os.path.basename(test_data['A_path'][0])
+                img_name, ext = os.path.splitext(image_name_ext)
+
+                    # img_dir = os.path.join(opt['path']['images'], img_name)
+                util.mkdir(img_dir)
+
+                model.feed_data(test_data, phase='test')
+                A_image,B_image,fusion_image = model.test()
+                fusion_loss, loss_gradient, loss_l1 = testloss(A_image,B_image, fusion_image)
+                loss += fusion_loss
+                visuals = model.current_visuals(need_H=need_GT)
+                    # E_img = util.tensor2uint(visuals['E'])
+                img=visuals['E']
+                E_img = img.data.squeeze().float().clamp_(0, 1).cpu().numpy()
+                if E_img.ndim == 3:
+                    E_img = np.transpose(E_img, (1, 2, 0))
+                E_img=np.uint8((E_img*255.0).round())
+                save_img_path = os.path.join(img_dir, '{:s}.png'.format(img_name))
+                util.imsave(E_img, save_img_path)
+            a_loss=loss / idx
+            if a_loss<best_loss:
+                best_loss = a_loss
+                print("epoch:{} Best loss {} :".format(epoch, best_loss))
+                save_dir = opt['path']['models']
+                save_filename = '{}_{}.pth'.format(epoch, 'GG')
+                save_path = os.path.join(save_dir, save_filename)
+                # logger.info('Saving the model. Save path is:{}'.format(save_path))
+                model.save(epoch)
+
+
+
+if __name__ == '__main__':
+    main()
